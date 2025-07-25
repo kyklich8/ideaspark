2875d626df606aa68355f4d248c068b3c6fdaa53
kyklich8 <89277579+kyklich8@users.noreply.github.com>
2025-07-25 14:32:44 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Добро пожаловать в IdeaSpark!</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
